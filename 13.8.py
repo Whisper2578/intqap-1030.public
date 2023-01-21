bc208361c42d6f33ffe0836100f16bac867708f8
Antonov Andrey <114511726+Whisper2578@users.noreply.github.com>
2023-01-21 11:51:17 +0300

    Update 13.8.py

--- 13.8.py
+++ 13.8.py
@@ -1,33 +1,38 @@
+def age(owners_age):
+    total_price=0
+    if 0 < owners_age < 18:
+        print('До 18 лет билет бесплатный! ')
+    elif 18 <= owners_age < 25:
+        total_price += 990
+        print('Стоимость билета: 990 руб. ')
+    else:
+        total_price += 1390
+        print('Стоимость билета: 1390 руб. ')
+    return total_price
 total_price = 0
 while True:
     try:
-        ticket_counter = int(input('Сколько билетов хотите приобрести?: '))
+        ticket_counter = int(input("Введите количество билетов: "))
         if type(ticket_counter) == int:
             break
     except ValueError:
         print('Введите целое число!: ')
-for i in range(ticket_counter):
-    i += 1
-    while True:
-        try:
-            owners_age = int(input(f'Укажите возраст владельца билета №{i}? '))
-            if 0 < owners_age < 18:
-                print('До 18 лет билет бесплатный! ')
-            elif owners_age <= 0:
-                print('Не верное значение! ')
-                ticket_counter = ticket_counter - 1
-            elif 18 <= owners_age < 25:
-                total_price += 990
-                print('Стоимость билета: 990 руб. ')
-            else:
-                total_price += 1390
-                print('Стоимость билета: 1390 руб. ')
-            if type(owners_age) == int:
+i = 0
+while True:
+    try:
+        owners_age = int(input("Введите возраст:"))
+        if owners_age <= 0 or owners_age > 100:
+            raise ValueError()
+        else:
+            i += 1
+            total_price += age(owners_age)
+            if i == ticket_counter:
                 break
-        except ValueError:
-            print('Введите целое число! ')
+        print("Билет №" + str(i))
+    except ValueError:
+            print('Введите целое число от 1 до 100 ')
 if ticket_counter > 3:
     total_price = total_price - ((total_price / 100) * 10)
-    print(f'Итого, с учётом 10% скидки при регистрации более 3-х человек, к оплате {total_price} руб. ')
+    print("Итого, с учётом 10% скидки при регистрации более 3-х человек, к оплате " + str(total_price) + " руб.")
 else:
-    print(f'Итого к оплате {total_price} руб.')
+    print("Итого к оплате " + str(total_price) + " руб.")
